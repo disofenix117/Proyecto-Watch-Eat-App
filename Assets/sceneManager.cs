b59bb23dfc6f71b56e856c29f28497c3dfbef1ca
Diego Suarez <disofenix117@hotmail.com>
2021-04-16 22:10:46 -0500

    Menu Hamburguesas

--- Assets/sceneManager.cs
+++ Assets/sceneManager.cs
@@ -1,9 +1,10 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+
+public class sceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,9 +16,7 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
-
     }
-
     public void cargarScene(string scene)
     {
         SceneManager.LoadScene(scene);
