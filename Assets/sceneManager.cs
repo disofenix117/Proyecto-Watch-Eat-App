8d6b1bbe99d25463b86e817e72c1f845761fcf3e
Diego Suarez <disofenix117@hotmail.com>
2021-04-16 22:51:25 -0500

    Menus conectados
    primera conexion principal de navegacion entre los menus

--- Assets/sceneManager.cs
+++ Assets/sceneManager.cs
@@ -6,19 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class sceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
     public void cargarScene(string scene)
     {
+        print(scene);
         SceneManager.LoadScene(scene);
 
     
