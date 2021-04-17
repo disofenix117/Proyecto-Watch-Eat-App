7a941dbac261d7a631a75136decf4c3a3c5a2b48
Diego Suarez <disofenix117@hotmail.com>
2021-04-17 10:58:59 -0500

    continuacion de menus
    se agrega menu auxiliar y escena "about"

---
+++ Assets/Timer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+   public float tiempo=300.0f;
+   public void Update()
+   {
+       tiempo=tiempo-1;
+       if(tiempo==0)
+       {
+           SceneManager.LoadScene("Menu");
+       }
+   }
+}
--- Assets/sceneManager.cs
+++ Assets/sceneManager.cs
@@ -6,10 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class sceneManager : MonoBehaviour
 {
+        public  GameObject panel;
+        bool act;
+void Start()
+    {
+        //panel=GameObject.Find("PanelAux");
+        act=false;
+        
+        
+    }
+    public void Panelaux()
+    {
+        act=!act;
+        print(act);
+        
+        if (act==false)
+        {
+            panel.SetActive(false);
+
+        }
+        else
+        {
+
+            panel.SetActive(true);
+        }
+     
+        
+
+    }
    
     public void cargarScene(string scene)
     {
         print(scene);
+        
         SceneManager.LoadScene(scene);
 
     
