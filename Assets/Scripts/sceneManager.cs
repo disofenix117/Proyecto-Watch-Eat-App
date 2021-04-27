435ebb1e2bb17a15847a008f7c1b4672162ccd13
disofenix117 <disofenix117@hotmail.com>
2021-04-27 12:30:28 -0500

    acualizacion

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sceneManager : MonoBehaviour
+{
+        public  GameObject panel;
+        bool act;
+void Start()
+    {
+        //panel=GameObject.Find("PanelAux");
+        act=false;
+        
+        
+    }
+    public void Panelaux()
+    {
+        act=!act;
+        print(act);
+        
+        if (act==false)
+        {
+            panel.SetActive(false);
+
+        }
+        else
+        {
+
+            panel.SetActive(true);
+        }
+     
+        
+
+    }
+   
+    public void cargarScene(string scene)
+    {
+        print(scene);
+        
+        SceneManager.LoadScene(scene);
+
+    
+    }
+}
