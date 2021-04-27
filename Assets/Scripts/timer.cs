cd618fc7f1b711836f764304e96f1f83cf9295a6
disofenix117 <disofenix117@hotmail.com>
2021-04-27 14:57:04 -0500

    Corrigiendo menus y modelos

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+        public float tiempo =0.0f;
+
+        public void Update()
+        {
+            tiempo+=tiempo+1;
+            if(tiempo>3000)
+            {
+                 SceneManager.LoadScene("Menu");
+
+            }
+        }
+}
